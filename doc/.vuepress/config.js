a477f55e3131603365c80d945404e5f52cf6add5
东方赞 <787814104@qq.com>
2022-10-09 20:06:55 +0800

    加入github

--- doc/.vuepress/config.js
+++ doc/.vuepress/config.js
@@ -30,7 +30,8 @@ module.exports = {
         lastUpdated: '最后更新',
         nav: [
             { text: '首页', link: '/' },
-            { text: 'Gitee', link: 'https://gitee.com/mortise-and-tenon/tesla-java-sdk' }
+            { text: 'Gitee', link: 'https://gitee.com/mortise-and-tenon/tesla-java-sdk' },
+            { text: 'Github', link: 'https://github.com/dongfangzan/tesla-java-sdk' }
         ],
         sidebar: [
             {
